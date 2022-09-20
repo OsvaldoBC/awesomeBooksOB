d8c035fd54a3a67dae015ba50bdf01e7aab96959
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2022-09-20 16:56:12 -0500

    add classes

--- src/js/main.js
+++ src/js/main.js
@@ -5,54 +5,58 @@ const author = document.getElementById('bookAuthor');
 const listContainer = document.querySelector('.booksList');
 let books = [];
 
-// a function to create items template
-function createElem({ title, author }) {
-  return `
+// Object Constructor functio
+
+class Book {
+  constructor(title, bookAuthor) {
+    this.title = title;
+    this.author = author;
+  }
+
+  // a function to create items template
+  createElem({ title, author }) {
+    return `
         <div class="list-item">
             <span>${title}</span>
             <span>${author}</span>
             <button class="remove-button">Remove</button>
         </div>`;
-}
+  }
 
-// Check for available data at local storage and displaying them
-function checkStorage() {
-  const storage = localStorage.getItem('books');
-  if (storage) {
-    books = JSON.parse(storage);
-    books.forEach((item) => {
-      listContainer.innerHTML += createElem(item);
-    });
-    const removeBtn = document.querySelectorAll('.remove-button');
-    removeBtn.forEach((e, i) => {
-      e.addEventListener('click', () => {
-        e.parentNode.remove();
-        books.splice(i, 1);
-        localStorage.setItem('books', JSON.stringify(books));
+  // Check for available data at local storage and displaying the
+  checkStorage() {
+    const storage = localStorage.getItem('books');
+    if (storage) {
+      books = JSON.parse(storage);
+      books.forEach((item) => {
+        listContainer.innerHTML += Book.createElem(item);
       });
-    });
+      const removeBtn = document.querySelectorAll('.remove-button');
+      removeBtn.forEach((e, i) => {
+        e.addEventListener('click', () => {
+          e.parentNode.remove();
+          books.splice(i, 1);
+          localStorage.setItem('books', JSON.stringify(books));
+        });
+      });
+    }
   }
 }
 
-// Object Constructor function
-function Book(title, author) {
-  this.title = title;
-  this.author = author;
-}
+// Load the data in the local storag
 
-// Load the data in the local storage
 window.addEventListener('load', () => {
-  checkStorage();
-});
+    Book.checkStorage();
+  });
 
 // on the form submit new item will be generated
 form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const bookTitle = title.value;
-  const bookAuthor = author.value;
-  const item = new Book(bookTitle, bookAuthor);
-  books.push(item);
-  localStorage.setItem('books', JSON.stringify(books));
-  listContainer.innerHTML = '';
-  checkStorage();
-});
+    e.preventDefault();
+    const bookTitle = Book.title.value;
+    const bookAuthor = Book.author.value;
+    const item = new Book(bookTitle, bookAuthor);
+    books.push(item);
+    localStorage.setItem('books', JSON.stringify(books));
+    listContainer.innerHTML = '';
+    Book.checkStorage();
+  });
