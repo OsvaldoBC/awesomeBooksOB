ddf5925829f515a66ec7d8ad6794b64083cf6d43
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2022-09-20 20:11:10 -0500

    Add Classes to Js File

--- src/js/main.js
+++ src/js/main.js
@@ -1,62 +1,120 @@
-// DOM Address to run handel events
-const form = document.querySelector('form');
-const title = document.getElementById('bookTitle');
-const author = document.getElementById('bookAuthor');
-const listContainer = document.querySelector('.booksList');
-let books = [];
+/* eslint-disable max-classes-per-file */
 
-// Object Constructor functio
-
+// Book class: Represents a book
 class Book {
-  constructor(title, bookAuthor) {
+  constructor(title, author, id) {
     this.title = title;
     this.author = author;
-  }
-
-  // a function to create items template
-  createElem({ title, author }) {
-    return `
-        <div class="list-item">
-            <span>${title}</span>
-            <span>${author}</span>
-            <button class="remove-button">Remove</button>
-        </div>`;
-  }
-
-  // Check for available data at local storage and displaying the
-  checkStorage() {
-    const storage = localStorage.getItem('books');
-    if (storage) {
-      books = JSON.parse(storage);
-      books.forEach((item) => {
-        listContainer.innerHTML += Book.createElem(item);
-      });
-      const removeBtn = document.querySelectorAll('.remove-button');
-      removeBtn.forEach((e, i) => {
-        e.addEventListener('click', () => {
-          e.parentNode.remove();
-          books.splice(i, 1);
-          localStorage.setItem('books', JSON.stringify(books));
-        });
-      });
-    }
+    this.id = id;
   }
 }
 
-// Load the data in the local storag
+// store class: Handles storage
+class store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
 
-window.addEventListener('load', () => {
-    Book.checkStorage();
-  });
+    return books;
+  }
 
-// on the form submit new item will be generated
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const bookTitle = Book.title.value;
-    const bookAuthor = Book.author.value;
-    const item = new Book(bookTitle, bookAuthor);
-    books.push(item);
+  static addBook(book) {
+    const books = store.getBooks();
+
+    books.push(book);
     localStorage.setItem('books', JSON.stringify(books));
-    listContainer.innerHTML = '';
-    Book.checkStorage();
-  });
+  }
+
+  static removeBook(id) {
+    const books = store.getBooks();
+
+    books.forEach((book, index) => {
+      if (book.id === id) {
+        books.splice(index, 1);
+      }
+    });
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+
+// UI class: Handles UI Task
+class UI {
+  static displayBooks() {
+    const books = store.getBooks();
+    books.forEach((book) => UI.addBookToList(book));
+  }
+
+  static addBookToList(book) {
+    const list = document.querySelector('.booksList');
+    const section = document.createElement('section');
+    section.classList.add('list-holder');
+
+    section.innerHTML = `<ul class="book-section">
+      <div class="book-caption">
+          <li class="title">"${book.title}"</li>
+          <li class="by">by</li>
+          <li class="author">${book.author}</li>
+          <li class="id">${book.id}</li>
+      </div>
+      <div>
+          <li>
+          <button class="del">Remove</button>
+          </li>
+      </div>
+  </ul>
+  `;
+    list.appendChild(section);
+  }
+
+  static deleteBook(el) {
+    if (el.classList.contains('del')) {
+      el.parentElement.parentElement.parentElement.parentElement.remove();
+    }
+  }
+
+  static clearFields() {
+    document.querySelector('#BookTitle').value = '';
+    document.querySelector('#BookAuthor').value = '';
+  }
+}
+
+// Event: Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // Get values from form
+  const title = document.querySelector('#bookTitle').value;
+  const author = document.querySelector('#bookAuthor').value;
+
+  // Generate random number per seconds
+  const id = Date.now().toString();
+
+  // Instantiate book
+  const book = new Book(title, author, id);
+
+  // Add book to UI
+  UI.addBookToList(book);
+
+  // Add book to store
+  store.addBook(book);
+
+  // clear fields
+  UI.clearFields();
+});
+
+// Event: Remove a book
+document.querySelector('.booksList').addEventListener('click', (e) => {
+  const id = document.querySelector('.id');
+
+  // Remove book from UI
+  UI.deleteBook(e.target);
+
+  // Remove from store
+  store.removeBook(id.textContent);
+});
